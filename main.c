37464e361d939714a9e1992bf03c50fed750b3ff
mmiah00 <mmiah00@stuty.edu>
2019-11-10 15:21:10 -0500

    wrote othr functions

--- main.c
+++ main.c
@@ -12,13 +12,33 @@ int randNumber () {
 	return ans;
 }
 
+void printArray (int arr[]){ 
+	int i; 
+	for (i = 0; i < 10; i ++) { 
+		printf ("\trandom %d: %d\n", i, arr[i]); 
+	}
+}
+
 int main () { 
 	int a [10]; 
 	printf ("Generating random numbers:\n"); 
 	int i; 
 	for (i = 0; i < 10; i ++) { 
 		a[i] = randomNumber (); 
-		printf ("\t random %d: %d\n", i, a[i]);  
 	}
+	printArray (a); 
+
+	printf ("Writing numbers to another file... \n"); 
+	int f = open ("numbers.txt", O_RDWR, 0666); 
+	write (f, a, sizeof (a)); 
+	
+	printf ("Reading numbers from the file ... \n"); 
+	int another [10]; 
+	int f2 = open ("numbers.txt", O_RDONLY); 
+	read (f2, &another, sizeof (another); 
+	
+	printf ("Verification that written values stay the same: \n"); 
+	printArray (another); 	
+
 	return 0;
 }
--- randfile.h
+++
@@ -1,6 +0,0 @@
-int randnumber(); 
-void populateArray (int a[]); 
-void arrayToFile (int a[]); 
-void readFile (file f);
-void printArry (int a[]); 
-
