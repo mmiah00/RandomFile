09fae0b25c25f323b88ca10ea86c6cbab579c27f
mmiah00 <mmiah00@stuty.edu>
2019-11-10 13:58:28 -0500

    starteD

---
+++ randfile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include "randfile.h"
+
+int randnumber () { 
+}
+
+void populateArray (int a []) { 
+}
+
+void arrayToFile (int a[]) { 
+}
+
+void readFile (file f) { 
+}
+
+void printArray (int a []) { 
+}
+
